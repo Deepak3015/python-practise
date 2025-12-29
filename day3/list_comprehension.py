be1c47d44a96afdc5141d836d587a8056e567cee
Deepak3015 <d03748651@gmail.com>
2025-12-29 13:43:33 +0530

    day4

--- day3/list_comprehension.py
+++ day3/list_comprehension.py
@@ -1,19 +1,30 @@
-# maker= []
-# for i in range(1,11):
-#     if i%2==0:
+# # maker= []
+# # for i in range(1,11):
+# #     if i%2==0:
 
-#         maker.append(i)
-# print(maker)
+# #         maker.append(i)
+# # print(maker)
 
-# maker = [i if i%2==0 else None for i in range (1,11) ]
-# print(maker) 
+# # maker = [i if i%2==0 else None for i in range (1,11) ]
+# # print(maker) 
 
-labour = {"mahesh":500,"ramesh":400,"mithilesh":400,"sumesh":300,"jagmohan":1000,"rampyare":800}
-total = 0
-days = 50
+# labour = {"mahesh":500,"ramesh":400,"mithilesh":400,"sumesh":300,"jagmohan":1000,"rampyare":800}
+# total = 0
+# days = 50
 
-for cost in labour:
-    total +=labour[cost]
+# for cost in labour:
+#     total +=labour[cost]
 
-total = (total*days)-((7*labour["mahesh"])+(4*labour["jagmohan"]))
-print(total)
+# total = (total*days)-((7*labour["mahesh"])+(4*labour["jagmohan"]))
+# print(total)
+
+
+data = {"DERF":0,"POENKN":10,"DD":7,"MAINDATA":[{"IDD":"d3454355","BDD":"5678hfjhjh","LINKID":4,"HeaderFields":[{"FieldTypeName":"H1","Value":"false"},{"FieldTypeName":"H2","Value":"148877564"},{"FieldTypeName":"H3","Value":"20230930"},{"FieldTypeName":"H4","Value":"20231130"},{"FieldTypeName":"H5","Value":"2441.56"},{"FieldTypeName":"H6","Value":"0.00"},{"FieldTypeName":"H7","Value":"2411.56"},{"FieldTypeName":"H8","Value":"EUR"},{"FieldTypeName":"H9","Value":"00115190035"},{"FieldTypeName":"H10","Value":""},{"FieldTypeName":"H11","Value":"4500575382"},{"FieldTypeName":"H12","Value":"0.00"},{"FieldTypeName":"H13","Value":""},{"FieldTypeName":"H14","Value":""},{"FieldTypeName":"H15","Value":"F0"},{"FieldTypeName":"H16","Value":"87"},{"FieldTypeName":"H17","Value":"0.00"},{"FieldTypeName":"H18","Value":""},{"FieldTypeName":"H19","Value":""},{"FieldTypeName":"H20","Value":"No"}],"CodingLines":[],"Tables":[{"N1":"233553","N2":"3555","N3":"ASDDDD","N4":"334324","N5":"900.00","N6":"1.29","N7":"387.00","N8":"","N9":"0.00","N10":"","N11":"","N12":"","N13":"","N14":""},{"N1":"765765","N2":"67657657","N3":"ADFDFF)","N4":"667657","N5":"1000.00","N6":"1.94","N7":"1940.00","N8":"","N9":"0.00","N10":"","N11":"","N12":"","N13":"","N14":""},{"N1":"67657","N2":"76576576576","N3":"SFDFFDFSDF","N4":"7667676","N5":"1000.00","N6":"0.11456","N7":"114.56","N8":"","N9":"0.00","N10":"","N11":"","N12":"","N13":"","N14":""}],"INININ":"148877564","SDRER":"null"},{"IDD":"frret5","BDD":"5trtry4566","LINKID":4,"HeaderFields":[{"FieldTypeName":"H1","Value":"false"},{"FieldTypeName":"H2","Value":"ICI2300397"},{"FieldTypeName":"H3","Value":"20231219"},{"FieldTypeName":"H4","Value":"20240331"},{"FieldTypeName":"H5","Value":"76.44"},{"FieldTypeName":"H6","Value":"0.00"},{"FieldTypeName":"H7","Value":"76.44"},{"FieldTypeName":"H8","Value":"INR"},{"FieldTypeName":"H9","Value":"56676765"},{"FieldTypeName":"H10","Value":""},{"FieldTypeName":"H11","Value":"0.00"},{"FieldTypeName":"H12","Value":""},{"FieldTypeName":"H13","Value":""},{"FieldTypeName":"H14","Value":"F1"},{"FieldTypeName":"H15","Value":"87"},{"FieldTypeName":"H16","Value":"0.00"},{"FieldTypeName":"H17","Value":""},{"FieldTypeName":"H18","Value":""}],"CodingLines":[{"CODE1":0.0,"CODE2":76.44,"CODE3":"5645654","CODE4":"","CodingFields":[{"FieldName":"COL1","Value":"223DD666"},{"FieldName":"COL2","Value":"3454545"},{"FieldName":"COL3","Value":""},{"FieldName":"COL5","Value":""},{"FieldName":"COL5","Value":""}]}],"Tables":[],"INININ":"DER3434","SDRER":"null"}],"Final":"JKHJKLH0909908"}
+# # for me in data:
+# first_dict = data["MAINDATA"]
+# print(first_dict[0])
+# print(data["MAINDATA"][0]["IDD"])
+# print(data["MAINDATA"][0]["IDD"])
+# print(type(data["MAINDATA"][0]["IDD"]))
+for keys in data["MAINDATA"][0]["HeaderFields"]:
+    print(keys)
